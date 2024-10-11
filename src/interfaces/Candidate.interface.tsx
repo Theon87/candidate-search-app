e71fc73f9de38a950d6e99beb21eaad3f0221115
theon87 <theonfrancis17@gmail.com>
2024-10-11 05:17:09 -0400

    adds an interface for the Candidate objects returned by the API

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    imageUrl: string;
+    name: string;
+    location: string;
+    email: string;
+    company: string;
+    bio: string;
+    hireable: boolean;    
+}
