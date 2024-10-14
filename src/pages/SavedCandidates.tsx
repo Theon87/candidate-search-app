2e1318c851cf12df9093a66cdabde9b07dbe2271
theon87 <theonfrancis17@gmail.com>
2024-10-14 00:25:30 -0400

    adds table for saved potential candidates

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,10 +1,15 @@
+// import CandidateSearch from "./CandidateSearch";
+// import { Candidate } from "../types";
+// import { useState } from "react";
+
 const SavedCandidates = () => {
-  // create a table to display the potential candidates in a table format
-  
 
-  // create a function to load the potential candidates from local storage
+// const [tableData, setTableData] = useState([{}]); 
 
-  // create a function to remove a potential candidate from the list of potential candidates
+// const [newRow, setNewRow] = useState<Candidate | null>(null);
+
+// add a new row to the table based on the saved data in local storage
+
 
   return (
     <>
@@ -12,8 +17,30 @@ const SavedCandidates = () => {
       {/*WHEN the potential candidates page loads
         THEN the user should see a list of previously saved potential
         candidates with their name, username, location, avatar, email, html_url, and company*/}
-
-
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Userame</th>
+              <th>Location</th>
+              <th>Avatar</th>
+              <th>Email</th>
+              <th>html_url</th>
+              <th>Company</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>candidate.name</td>
+              <td>candidate.username</td>
+              <td>candidate.location</td>
+              <td>candidate.avatar_url</td>
+              <td>candidate.email</td>
+              <td>candidate.html_url</td>
+              <td>candidate.company</td>
+            </tr>
+          </tbody>
+        </table>
 
       {/*WHEN the page reloads
         THEN the list of potential candidates should persist and be available for viewing*/}
@@ -21,7 +48,6 @@ const SavedCandidates = () => {
       
       {/*WHEN there are no potential candidates
         THEN an appropriate message should be displayed indicating no candidates have been accepted*/}
-      
 
     </>
   );
